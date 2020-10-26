447095b1f8da6df986ba79ee108ea94ecf30f968
Abdallah Hanfy <abdallahhanfy@outlook.com>
2020-10-27 01:05:17 +0200

    add custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
+      theme: ThemeData(primarySwatch: Colors.purple, accentColor: Colors.amber),
       home: MyHomePage(),
     );
   }
